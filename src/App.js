3630e862389b58adcd97fa70370ee10586ffb56f
Zlata <zlata@ellery.ru>
2020-07-06 15:23:51 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import Table from './components/Table'
 import './App.css';
+
+const multiply = (n) => {
+  const result = {};
+  for (let i = 1; i <= n; i++) {
+    result[i] = {};
+    for (let j = 1; j <= n; j++) {
+      result[i][j] = i * j
+    }
+  }
+  console.table(result)
+}
+
+multiply(20);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Table />
     </div>
   );
 }
---
+++ src/components/Table.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TableItem from './TableItem'
+
+const Table = () => {
+  return (
+    <table>
+       <caption>Multiplication table</caption>
+
+     </table>
+  )
+}
+
+export default Table
